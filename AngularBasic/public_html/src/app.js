c74ce801e3341809fa1bf3f8eb68ceacdad6e34a
kaosterra <kaosterra@gmail.com>
2015-02-17 09:59:42 -0500

    Se añade versión básica de country

--- AngularBasic/public_html/src/app.js
+++ AngularBasic/public_html/src/app.js
@@ -1,10 +1,12 @@
 (function () {
 
-	var mainApp = angular.module('mainApp', ['ngRoute', 'sportModule']);
+	var mainApp = angular.module('mainApp', ['ngRoute', 'sportModule','countryModule']);
 
 	mainApp.config(['$routeProvider', function ($routeProvider) {
 			$routeProvider.when('/sport', {
 				templateUrl: 'src/modules/sport/sport.tpl.html'
+			}).when('/country', {
+				templateUrl: 'src/modules/country/country.tpl.html'
 			}).otherwise('/');
 		}]);
 
@@ -14,6 +16,15 @@
 	sportModule.constant('sport.context', 'sports');
 
 	sportModule.config(['sport.context', 'MockModule.urlsProvider', function (context, urlsProvider) {
+			urlsProvider.registerUrl(context);
+		}]);
+	
+	//Configuración módulo sport
+	var sportModule = angular.module('countryModule', ['CrudModule', 'MockModule']);
+
+	sportModule.constant('country.context', 'countries');
+
+	sportModule.config(['country.context', 'MockModule.urlsProvider', function (context, urlsProvider) {
 			urlsProvider.registerUrl(context);
 		}]);
 })();
